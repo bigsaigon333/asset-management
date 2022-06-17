446c0ded1c83840295341c60b29cde57c86942be
Donghee Kim <dongheekim@woowahan.com>
2022-06-17 20:44:08 +0900

    feat: 로그아웃 후 토스트 메세지를 보여준다

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,9 +1,14 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-import { signIn as firebaseSignIn } from "../firebase";
+import {
+  signIn as firebaseSignIn,
+  signOut as firebaseSignOut,
+} from "../firebase";
+import { useToast } from "@chakra-ui/react";
 
 interface AuthContext {
   isSignIn: boolean;
   signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
 }
 
 const authContext = createContext<AuthContext | null>(null);
@@ -12,17 +17,43 @@ authContext.displayName = "authContext";
 export const AuthContextProvider = ({ children }: { children?: ReactNode }) => {
   const [isSignIn, setIsSignIn] = useState(false);
 
+  const toast = useToast();
+
   const signIn = async () => {
     try {
       const user = await firebaseSignIn();
       setIsSignIn(true);
     } catch (error) {
       console.error(error);
+      toast({
+        title: "로그인에 실패하였습니다.",
+        status: "error",
+        position: "top",
+      });
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await firebaseSignOut();
+      setIsSignIn(false);
+      toast({
+        title: "로그아웃되었습니다",
+        status: "success",
+        position: "top",
+      });
+    } catch (error) {
+      console.error();
+      toast({
+        title: "로그아웃에 실패하였습니다.",
+        status: "error",
+        position: "top",
+      });
     }
   };
 
   return (
-    <authContext.Provider value={{ isSignIn, signIn }}>
+    <authContext.Provider value={{ isSignIn, signIn, signOut }}>
       {children}
     </authContext.Provider>
   );
