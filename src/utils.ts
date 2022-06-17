94b27e5a133fa5a4f369649b498fe2ee22980148
Donghee Kim <dongheekim@woowahan.com>
2022-06-17 22:25:47 +0900

    feat: toJSON, parse 를 구현한다

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,31 @@ export const formatRate = (rate: number): string => {
 };
 
 export const add = (a: number, b: number): number => a + b;
+
+export const toJSON = (value: unknown): any => {
+  if (value !== null && typeof value === "object") {
+    if (Array.isArray(value)) {
+      return value.map((el) => toJSON(el));
+    } else {
+      // @ts-ignore
+      if ("toJSON" in value && typeof value.toJSON === "function") {
+        // @ts-ignore
+        return value.toJSON();
+      } else {
+        return Object.fromEntries(
+          Object.entries(value).map(([key, val]) => [key, toJSON(val)])
+        );
+      }
+    }
+  } else {
+    return value;
+  }
+};
+
+export const parse = <T>(value: string): T => {
+  return JSON.parse(value, (key, value) =>
+    typeof value === "string" && !Number.isNaN(Date.parse(value))
+      ? new Date(Date.parse(value))
+      : value
+  );
+};
